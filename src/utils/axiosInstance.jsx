6b0d5e71a607eefda25b1590e66592be79468770
Paulus <pwilhem@mtc.com.na>
2025-10-20 16:59:02 +0200

    verification

--- src/utils/axiosInstance.jsx
+++ src/utils/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:4000',
-   baseURL: 'http://172.19.13.140:4000',
+  baseURL: 'http://localhost:4000',
+   //baseURL: 'http://172.19.13.140:4000',
   headers: { 'Content-Type': 'application/json' },
 });
 
